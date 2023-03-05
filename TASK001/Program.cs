4ae5980a9e45bd72ec994f82af50383278ed9389
Marina Artemyeva <martemyeva@yahoo.com>
2023-03-05 15:11:29 +0600

    Correct code in TASK001

--- TASK001/Program.cs
+++ TASK001/Program.cs
@@ -5,15 +5,37 @@
 //1 -3,3 8 -9,9
 //8 7,8 -7,1 9
 
-double[,] a = new double[3, 4];
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
 
-Random random = new Random();
-for (int i = 0; i < 3; i++)
-{
-    for (int j = 0; j < 4; j++)
-    {
-        a[i, j] = random.NextDouble() * 10; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
-        Console.Write("{0,6:F2}", a[i, j]);
+double[,] twoDimArray = new double[m, n];
+Random rnd = new Random();
+
+void PrintArray(double[,] matr)
+{ 
+    for (int i = 0; i < m; i++)
+    { 
+        for (int j = 0; j < n; j++)
+        { 
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
+
+void FillArray(double[,] matr)
+{ 
+    for (int i = 0; i < m; i++)
+    { 
+        for (int j = 0; j < n; j++)
+        { 
+            matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);
+        }
+    }
+}
+
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
